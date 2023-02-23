48485d534cbf54dba392d2b3eb872686d167ac46
Philip Corliss <pcorliss@50projects.com>
2023-02-22 22:17:24 -0600

    unique ids working

--- maelstrom-uniqueid/main.go
+++ maelstrom-uniqueid/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
@@ -9,10 +10,13 @@ import (
 func main() {
 	n := maelstrom.NewNode()
 
+	var i int = 1
+
 	n.Handle("generate", func(msg maelstrom.Message) error {
-		var response map[string]any = make(map[string]any)
+		var response map[string]string = make(map[string]string)
 		response["type"] = "generate_ok"
-		response["id"] = 123
+		response["id"] = fmt.Sprintf("%s_%d", n.ID(), i)
+		i += 1
 
 		return n.Reply(msg, response)
 	})
