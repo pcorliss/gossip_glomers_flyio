435d52745df014804ccdb90f40de617c4cae1603
Philip Corliss <pcorliss@50projects.com>
2023-02-23 20:17:45 -0600

    Multi Node Broadcast with mutexes!

--- maelstrom-broadcast/main.go
+++ maelstrom-broadcast/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"os"
+	"sync"
 
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
@@ -11,6 +14,18 @@ func main() {
 	n := maelstrom.NewNode()
 
 	var messages []int = make([]int, 0)
+	var messageSet map[int]bool = make(map[int]bool)
+	var messageMutex = sync.RWMutex{}
+
+	type Topology map[string][]string
+	type TopologyMessage struct {
+		MsgType  string   `json:"type"`
+		Topology Topology `json:"topology"`
+	}
+
+	var topology Topology = nil
+
+	n.Handle("broadcast_ok", func(msg maelstrom.Message) error { return nil })
 
 	// {
 	// 	"type": "broadcast",
@@ -25,10 +40,34 @@ func main() {
 			return err
 		}
 
-		messages = append(messages, int(body["message"].(float64)))
-
 		var response map[string]string = make(map[string]string)
 		response["type"] = "broadcast_ok"
+
+		var num int = int(body["message"].(float64))
+		if messageSet[num] {
+			// If we already have this number skip re-broadcasting
+			return n.Reply(msg, response)
+		}
+
+		messageMutex.Lock()
+		messages = append(messages, int(body["message"].(float64)))
+		messageSet[num] = true
+		messageMutex.Unlock()
+
+		// rebroadcast to topology neighbors
+		// but exclude the sender
+		var neighbors = topology[n.ID()]
+		for _, neighbor := range neighbors {
+			if neighbor == msg.Src {
+				continue
+			}
+			n.Send(neighbor, msg.Body)
+			// n.RPC(neighbor, msg.Body, func(msg maelstrom.Message) error {
+			// 	var neighborResponse map[string]string = make(map[string]string)
+			// 	neighborResponse["type"] = "broadcast_ok"
+			// 	return n.Reply(msg, neighborResponse)
+			// })
+		}
 
 		return n.Reply(msg, response)
 	})
@@ -42,7 +81,9 @@ func main() {
 	n.Handle("read", func(msg maelstrom.Message) error {
 		var response map[string]any = make(map[string]any)
 		response["type"] = "read_ok"
+		messageMutex.RLock()
 		response["messages"] = messages
+		messageMutex.RUnlock()
 
 		return n.Reply(msg, response)
 	})
@@ -57,7 +98,16 @@ func main() {
 	//   {
 	// 	"type": "topology_ok"
 	//   }
+
 	n.Handle("topology", func(msg maelstrom.Message) error {
+		var body TopologyMessage
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		topology = body.Topology
+		fmt.Fprintf(os.Stderr, "topology: %v\n", topology)
+
 		var response map[string]string = make(map[string]string)
 		response["type"] = "topology_ok"
 
